b6f8a3461168a33a7e58d8a3e93a36874095bdbd
Earvin Li <earvin.tli@gmail.com>
2019-11-12 19:10:34 +0800

    [App] [Post] Add 'react-native-vector-icons' and use two vote icons

--- src/PostList/PostListCard.js
+++ src/PostList/PostListCard.js
@@ -1,4 +1,5 @@
 // External Dependencies
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import React from 'react';
 import { Card } from 'react-native-elements';
 import {
@@ -35,6 +36,8 @@ const PostListCard = (props) => {
         source={{ uri: "https://images.unsplash.com/photo-1514911834724-fbe785fc8a6a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=320&h=240&fit=crop&ixid=eyJhcHBfaWQiOjF9" }}
         style={imageStyle}
       />
+      <MaterialCommunityIcons name="arrow-up-bold" />
+      <MaterialCommunityIcons name="arrow-down-bold" />
     </Card>
   );
 };
